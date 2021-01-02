9996b18319ff80a712516b4ec9a8994df5e7523b
hiromi-mi <hiromi-mi@cat.zaq.jp>
2021-01-02 20:25:04 +0900

    work on gnu/linux

--- polyglot/polyglot-ruby-bash-quine.rb
+++ polyglot/polyglot-ruby-bash-quine.rb
@@ -1,13 +1,13 @@
-"alias" "puts=f()(gtee - /dev/stdout;echo '# #';rev -|gtac|gtee /dev/stdout);f" #
+"alias" "puts=f()(tee x;cat x;echo '# #';rev x|tac;rev x|tac);f" #
 eval "#{class Object def revp() self.to_s*2+%Q(# #)+self.to_s.reverse*2 end end}" #
 puts <<'# #' .revp #
-"alias" "puts=f()(gtee - /dev/stdout;echo '# #';rev -|gtac|gtee /dev/stdout);f" #
+"alias" "puts=f()(tee x;cat x;echo '# #';rev x|tac;rev x|tac);f" #
 eval "#{class Object def revp() self.to_s*2+%Q(# #)+self.to_s.reverse*2 end end}" #
 puts <<'# #' .revp #
 # #
 # pver. '# #'<< stup
 # "}dne dne 2*esrever.s_ot.fles+)# #(Q%+2*s_ot.fles )(pver fed tcejbO ssalc{#" lave
-# "f;)tuodts/ved/ eetg|catg|- ver;'# #' ohce;tuodts/ved/ - eetg()(f=stup" "saila"
+# "f;)tuodts/ved/ eetg|catg|x ver;'# #' ohce;tuodts/ved/ x eetg()(f=stup" "saila"
 # pver. '# #'<< stup
 # "}dne dne 2*esrever.s_ot.fles+)# #(Q%+2*s_ot.fles )(pver fed tcejbO ssalc{#" lave
-# "f;)tuodts/ved/ eetg|catg|- ver;'# #' ohce;tuodts/ved/ - eetg()(f=stup" "saila"
+# "f;)tuodts/ved/ eetg|catg|x ver;'# #' ohce;tuodts/ved/ x eetg()(f=stup" "saila"
